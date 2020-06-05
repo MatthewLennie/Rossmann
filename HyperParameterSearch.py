activations output to png, normalization problem fixed

HyperParameterSearch.py
@@ -16,6 +16,7 @@ import logging
 import numpy as np
 from typing import List, Tuple
 import torch.cuda.profiler as profiler
+import matplotlib.pyplot as plt
 
 
 def setup_run(
@@ -60,7 +61,9 @@ def setup_run(
         cosine_annealing_period, lr, betas
     )
     example_runner: Runner = Runner(rossman_learner, [cb1, cb2, cb3, cb4, cb5])
-    example_runner.fit(600)
+    example_runner.fit(1)
+    plt.plot(cb4.lr)
+    plt.gcf().savefig("lr.png")
     del example_runner
     del cb1, cb2, cb3, cb4, cb5
 
@@ -76,7 +79,6 @@ valid_data_obj: RossmanDataset = RossmanDataset.from_pickle(
     "./data/valid_data.pkl"
 )
 
-
 # import_rossman_data.main()
 # with torch.autograd.profiler.emit_nvtx():
 # callback_handler.main()  # sets up a hyperparameter search of the model
@@ -87,19 +89,21 @@ logger = logging.getLogger(__name__)
 
 # Hyperparameter Search Range
 batch_size: List[int] = [250000]  # Maxes out the ram
-cosine_annealing_period: List[int] = [10, 5]
+cosine_annealing_period: List[int] = [40, 5]
 layer_sizes: List[List[int]] = [
     # [240, 1000, 50],  # <- Jeremy used this one in the course.
     # [240, 1000, 250, 50],
     [240, 150, 80, 40, 10],
     [60, 60, 40, 30, 20, 10],  # <-This one ended up worked well
 ]
-lr: List[float] = [0.001, 0.0005]
+# lr: List[float] = [0.01, 0.0005]
+lr: List[float] = [0.01]
 dropout: List[float] = [0.1, 0.3, 0.4]
 betas: List[List[float]] = [
     [0.9, 0.999],  # The normal default
     [0.99, 0.9999],
     [0.999, 0.99999],
+    [0.99999, 0.999999],
     [0.8, 0.99],
 ]
 

callback_handler.py
@@ -8,6 +8,10 @@ import coloredlogs
 import logging
 import math
 from FastTensorDataLoader import FastTensorDataLoader
+import re
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class CallBack:
@@ -110,19 +114,22 @@ class OptimizerCallBack(CallBack):
 
     def cosine_annealing(self, pos):
         relative_pos = pos % self.cosine_annealing_period
-        peak = 0.3 * self.cosine_annealing_period
-        start = 0.00001
+        peak = 0.6 * self.cosine_annealing_period
+        start = 0.0000001
 
         if relative_pos <= peak:
             return (
-                start
-                + (1 - math.cos(math.pi * relative_pos * 0.5 / peak)) * self.lr
+                start + (1 - math.cos(math.pi * relative_pos / peak)) * self.lr
             )
         else:
 
             return self.lr * (
                 1
-                + math.cos(math.pi * (relative_pos - peak) * 0.5 / (1 - peak))
+                + math.cos(
+                    math.pi
+                    * (relative_pos - peak)
+                    / (self.cosine_annealing_period - peak)
+                )
             )
 
     def model_set_up(self):
@@ -140,7 +147,7 @@ class OptimizerCallBack(CallBack):
             param_group["lr"] = new_lr
 
     def after_optim(self):
-        self.epoch += 1 / self.batches
+        self.run.epoch += 1 / self.batches
 
 
 class TensorBoardLogCallBack(CallBack):
@@ -156,6 +163,45 @@ class TensorBoardLogCallBack(CallBack):
         self.writer = SummaryWriter(
             "runs/{}".format(random.randint(0, int(1e9)))
         )
+        self.lr = []
+        self.hists = defaultdict(list)
+
+    def set_runner(self, run):
+        super().set_runner(run)
+        self.get_all_layers(self.run.learner.model)
+
+    def hook_fn(self, name):
+        def _inner(layer_object, input, output):
+            run = self.run
+            # print(name)
+            # self.writer.add_histogram("Mean" + name, output, run.epoch)
+            if run.training_mode:
+                hist = torch.histc(output, 200, -3, 3)
+                self.hists[name].append(hist / hist.sum())
+                del hist
+
+        return _inner
+
+    def torch_hooks_init(self):
+        self.get_all_layers(self.run.learner.mode)
+        return None
+
+    def get_all_layers(self, layer):
+        for name, layer in layer._modules.items():
+            # If it is a sequential, don't register a hook on it
+            # but recursively register hook on all it's module children
+            print(layer)
+            if isinstance(layer, torch.nn.ModuleList):
+                self.get_all_layers(layer)
+            else:
+                # it's a non sequential. Register a hook
+                print("registering {}".format(name))
+                p = re.compile("(^[a-zA-Z]*)")
+                m = p.match(str(layer))
+                layer.register_forward_hook(
+                    self.hook_fn("{}/{}".format(m.group(0), name))
+                )
+        print("hooks registered")
 
     def model_set_upTODO(self):
         # hyperparameter logging.
@@ -167,22 +213,36 @@ class TensorBoardLogCallBack(CallBack):
         self.hyperparameters["hparam/current_epoch"] = 0
         self.hyperparameters["dropout"] = self.dropout
 
-    def after_train_epoch(self):
-        self.writer.add_scalar("Training_Loss", self.train_loss, self.epoch)
+    def after_optim(self):
+        self.lr.append(self.learner.optim.param_groups[0]["lr"])
         self.writer.add_scalar(
             "lr", self.learner.optim.param_groups[0]["lr"], self.epoch
         )
 
+    def after_train_epoch(self):
+        self.writer.add_scalar("Training_Loss", self.train_loss, self.epoch)
+
     def after_validation(self):
         self.writer.add_scalar("Validation_Loss", self.valid_loss, self.epoch)
+
+    def after_fit(self):
+        for key in self.hists:
+            full = torch.stack(self.hists[key])
+            plt.imshow(
+                full.cpu().detach().numpy().transpose(),
+                extent=[0, full.shape[0], -3, 3],
+            )
+            plt.gca().set_yticks(np.linspace(-3, 3, 10))
+            plt.gcf().savefig(key)
+        del self.hists
 
 
 class Runner:
     def __init__(self, learner: rt.Learner, cbs: List[CallBack] = []):
         self.cbs = sorted(cbs, key=lambda k: k._order)
-        self.init_cbs()
         self.epoch = 0
         self.learner = learner
+        self.init_cbs()
         self.training_mode = True
         if self("model_set_up"):
             return
@@ -252,6 +312,8 @@ class Runner:
             self.valid_loss = self.do_all_batches(self.learner.valid_data)
             if self("after_validation"):
                 return
+        if self("after_fit"):
+            return
 
 
 def main():

rossman_tabular.py
@@ -14,6 +14,24 @@ import numpy as np
 from FastTensorDataLoader import FastTensorDataLoader
 
 
+class GeneralizedReLU(torch.nn.Module):
+    def __init__(self, leak=None, sub=None, maxv=None):
+        super().__init__()
+        self.leak, self.sub, self.maxv = leak, sub, maxv
+
+    def forward(self, x):
+        x = (
+            torch.nn.functional.leaky_relu(x, self.leak)
+            if self.leak is not None
+            else torch.nn.functional.relu(x)
+        )
+        if self.sub is not None:
+            x.sub_(self.sub)
+        if self.maxv is not None:
+            x.clamp_max_(self.maxv)
+        return x
+
+
 class TabularRossmanModel(torch.nn.Module):
     """[model class for rossman model, tabular style nn]
 
@@ -60,8 +78,10 @@ class TabularRossmanModel(torch.nn.Module):
         self.linear_layers = []
 
         for in_size, out_size in zip(layer_sizes[:-2], (layer_sizes[1:-1])):
-            self.linear_layers.append(torch.nn.Linear(in_size, out_size))
-            self.linear_layers.append(torch.nn.ReLU())
+            self.linear_layers.append(
+                torch.nn.Linear(in_size, out_size, bias=False)
+            )
+            self.linear_layers.append(GeneralizedReLU(0.1, 0.4))
             self.linear_layers.append(torch.nn.BatchNorm1d(out_size))
             self.linear_layers.append(torch.nn.Dropout(p=dropout))
 